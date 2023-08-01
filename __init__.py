81833a34ccf027a3f9de5c6c3674739ea2c29a41
ByteRookie <Jasonlightman@gmail.com>
2023-07-31 20:40:11 -0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -19,34 +19,54 @@ CONF_OVERRIDE_CONTENT = 'override_content'
 
 # Define the configuration schema for a device
 DEVICE_SCHEMA = vol.Schema({
-    vol.Required(CONF_DEVICE_ID): cv.string,
-    vol.Required(CONF_DEVICE_TYPE): cv.string,
-    vol.Required(CONF_PRIORITY): cv.positive_int,
-    vol.Optional(CONF_OVERRIDE_CONTENT): cv.string,
+    vol.Required("rooms"): vol.All(
+        cv.ensure_list,
+        [
+            vol.Schema(
+                {
+                    vol.Required("room"): cv.string,
+                    vol.Required("devices"): vol.All(
+                        cv.ensure_list,
+                        [
+                            vol.Schema(
+                                {
+                                    vol.Required("device_id"): cv.string,
+                                    vol.Required("device_type"): cv.string,
+                                    vol.Required("priority"): cv.positive_int,
+                                    vol.Optional("override_content"): cv.string,
+                                }
+                            )
+                        ],
+                    ),
+                }
+            )
+        ],
+    ),
+    vol.Required("Source_selector"): cv.string,
+    vol.Required("Sources"): vol.All(
+        cv.ensure_list,
+        [
+            vol.Schema(
+                {
+                    vol.Required("Source"): cv.string,
+                    vol.Required("Source_Value"): cv.string,
+                }
+            )
+        ],
+    ),
 })
-
-# Define the configuration schema for a room
-ROOM_SCHEMA = vol.Schema({
-    vol.Required(CONF_ROOM): cv.string,
-    vol.Required(CONF_DEVICES): vol.All(cv.ensure_list, [DEVICE_SCHEMA]),
-})
-
-# Define the configuration schema for the integration
-CONFIG_SCHEMA = vol.Schema({
-    DOMAIN: vol.Schema({
-        vol.Required(CONF_ROOMS): vol.All(cv.ensure_list, [ROOM_SCHEMA]),
-    })
-}, extra=vol.ALLOW_EXTRA)
-
-_LOGGER = logging.getLogger(__name__)
 
 async def async_setup(hass, config):
     """Set up the custom component."""
-    # Retrieve the room information from the configuration
-    rooms = config[DOMAIN][CONF_ROOMS]
 
-    # Store the room information in hass.data, which is a shared dictionary for storing data
-    hass.data[DOMAIN] = rooms
+
+    hass.data['ags_service'] = {
+        'rooms': config['ags_service']['rooms'],
+        'Source_selector': config['ags_service']['Source_selector'],
+        'Sources': config['ags_service']['Sources']
+    }
+    ...
+
 
     # Load the sensor and switch platforms and pass the configuration to them
     await async_load_platform(hass, 'sensor', DOMAIN, {}, config)
