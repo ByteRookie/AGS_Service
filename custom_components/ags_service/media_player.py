32c8ebb4abd25e16c5d94e0cf132b0b11d2bed1e
ByteRookie <Jasonlightman@gmail.com>
2025-07-11 15:58:35 -0700

    Fix lint warnings

--- custom_components/ags_service/media_player.py
+++ custom_components/ags_service/media_player.py
@@ -3,11 +3,11 @@ from homeassistant.components.media_player import MediaPlayerEntity
 from homeassistant.components.media_player.const import (
     MediaPlayerEntityFeature as MPFeature,
 )
-from homeassistant.const import STATE_IDLE, STATE_PLAYING, STATE_PAUSED
+from homeassistant.const import STATE_IDLE
 from homeassistant.helpers.event import async_track_state_change_event
 
 import asyncio
-from .ags_service import update_ags_sensors, ags_select_source, get_control_device_id
+from .ags_service import update_ags_sensors, ags_select_source
 
 import logging
 _LOGGER = logging.getLogger(__name__)
@@ -407,7 +407,7 @@ class AGSPrimarySpeakerMediaPlayer(MediaPlayerEntity, RestoreEntity):
         ags_config = self.hass.data['ags_service']
         disable_Tv_Source = ags_config['disable_Tv_Source']
 
-        if self.ags_status == "ON TV" and disable_Tv_Source == False:
+        if self.ags_status == "ON TV" and not disable_Tv_Source:
             sources = self.primary_speaker_state.attributes.get('source_list') if self.primary_speaker_state else None 
 
         else:
